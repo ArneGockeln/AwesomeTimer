69cf08531767af56f2d78d9e504fc162621336fc
ArneGockeln <arne@gockeln.com>
2025-09-04 17:08:01 +0200

    WIP

--- Tests/AwesomeTimerTests/AwesomeTimerTests.swift
+++ Tests/AwesomeTimerTests/AwesomeTimerTests.swift
@@ -4,14 +4,3 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
-
-@Test func testSecondCountdown() async throws {
-    let timer = AwesomeTimer()
-    var count = 4
-    timer.startCountdown(seconds: 4) { _ in 
-        count -= 1
-    }
-
-    // does not work. 
-    #expect(count == 0)
-}
